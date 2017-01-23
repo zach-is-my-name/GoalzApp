4c0d1096df65257e38c2765f096bb04a919d2886
zach-is-my-name <zach.greco@gmail.com>
2017-01-23 16:02:01 -0500

    Header Component Started, Project Scaffolding

--- client/js/index.js
+++ client/js/index.js
@@ -1,3 +1,9 @@
 import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
 console.log(`Client running in ${process.env.NODE_ENV} mode`);
+
+ReactDOM.render(
+  <App />, document.getElementById('app'));
