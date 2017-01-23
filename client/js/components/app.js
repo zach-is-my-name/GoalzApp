feed516f1de08426c4ca391a14c871b5e4afa2e4
zach-is-my-name <zach.greco@gmail.com>
2017-01-23 16:24:42 -0500

    Components

---
+++ client/js/components/app.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from './header'
+
+function App() {
+  return(
+
+    <div className="app">
+      <Header />
+    </div>
+)}
+export default App
---
+++ client/js/components/header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function Header() {
+  return (
+    <div className="Header">
+      <header>
+        <h1>
+          GoalzApp
+        </h1>
+      </header>
+    </div>
+)
+}
+
+export default Header;
